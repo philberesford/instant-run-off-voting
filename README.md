d6dec5c299616c01899e8496b39845ca918fd627
philberesford <philberesford>
2021-11-26 19:01:06 +0000

    Added usage instructions into the README and enabled greater re-use without having to modify the core of the application.

---
+++ README.md
@@ -0,0 +1,23 @@
+# Calculates the winner of an election using instant run-off voting
+
+## Example usage
+```bash
+pip install -r requirements.txt
+python app.py
+```
+
+## Data format
+Data must be passed through as two related lists of values:
+1. A list of strings containing the Candidates that can be voted for.
+2. A list of CSVs containing the Preferences (by number) of each Voter. 
+The position of the Preference in the CSV corresponds to the Candidate at the same position in the list of Candidates.
+
+## Assumptions
+1. The number of Preferences for each vote corresponds to the number of Candidates. 
+2. The Preference for each Candidate is unique for each Voter.
+
+## Usage
+The output from [Microsoft Forms](https://forms.office.com/) can output the values of single questions in a suitable format for use by this script.
+
+## Dependencies
+[pyrankvote](https://pypi.org/project/pyrankvote/)
--- instant_run_off.py
+++ instant_run_off.py
@@ -25,28 +25,3 @@ def get_result(candidate_names: List[str], voter_rankings_as_csv: List[str]):
     winners = election_result.get_winners()
     return election_result
 
-if __name__ == "__main__":
-    candidates = ["Sunday, 7pm until 9pm (In Person)",
-                  "Sunday, 7pm until 9pm (Zoom)",
-                  "Alternating Tuesday/Wednesday 7pm until 9pm (In Person)",
-                  "Alternating Tuesday/Wednesday 7pm until 9pm (Zoom)",
-                  "Friday 6pm until 8pm (In Person)",
-                  "Friday 6pm until 8pm (Zoom)",
-                  "Saturday 11am until 1pm (In Person)",
-                  "Saturday 11am until 1pm (Zoom)"
-                  ]
-
-    votes = ["8,7,1,2,6,5,3,4",
-             "5,6,1,2,3,4,7,8",
-             "5,6,1,2,7,8,3,4",
-             "2,4,3,5,8,7,1,6",
-             "3,4,5,2,6,1,8,7",
-             "2,1,6,5,4,3,8,7",
-             "2,1,7,8,6,5,3,4",
-             "6,5,1,2,4,3,7,8",
-             "3,4,1,2,6,5,7,8",
-             "4,3,2,1,8,5,6,7",
-             "2,1,4,3,8,7,5,6",
-             "1,2,3,4,5,6,7,8",
-            ]
-    print(get_result(candidates, votes))
