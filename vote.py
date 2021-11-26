1a3ef3ce0e3bbe8ccaf9b431ca7f7234fbbc0912
philberesford <philberesford>
2021-11-26 18:48:55 +0000

    Re-engineered the solution to make it easier to use should it be needed again.

--- vote.py
+++ vote.py
@@ -10,17 +10,31 @@ def create_ballots(candidates, rankings):
 
     return preferences
 
-def run():
-    sunday_person = Candidate("Sunday, 7pm until 9pm (In Person)")
-    sunday_zoom = Candidate("Sunday, 7pm until 9pm (Zoom)")
-    tuesday_person = Candidate("Alternating Tuesday/Wednesday 7pm until 9pm (In Person)")
-    tuesday_zoom = Candidate("Alternating Tuesday/Wednesday 7pm until 9pm (Zoom)")
-    friday_person = Candidate("Friday 6pm until 8pm (In Person)")
-    friday_zoom = Candidate("Friday 6pm until 8pm (Zoom)")
-    saturday_person = Candidate("Saturday 11am until 1pm (In Person)")
-    saturday_zoom = Candidate("Saturday 11am until 1pm (Zoom)")
+def get_result(candidate_names: List[str], voter_rankings_as_csv: List[str]):
+    """
+    Returns the winner of a vote using Instant Run-Off voting
+    :param candidate_names: A list of strings, containing the candidates. E.g. ["Option A", "Option B", "Option C"]
+    :param voter_rankings_as_csv: A list of CSVs, containing voter preferences. E.g. ["1,2,3", "2,3,1", "3,2,1"]
+    :return:
+    """
+    candidates = [Candidate(name) for name in candidate_names]
+    ranked_candidates = [create_ballots(candidates, map(int, ranking_str.split(","))) for ranking_str in voter_rankings_as_csv]
+    ballots = [Ballot(ranked_candidates=ranked_candidate) for ranked_candidate in ranked_candidates]
 
-    candidates = [sunday_person, sunday_zoom, tuesday_person,tuesday_zoom, friday_person, friday_zoom, saturday_person, saturday_zoom]
+    election_result = pyrankvote.instant_runoff_voting(candidates, ballots)
+    winners = election_result.get_winners()
+    return election_result
+
+if __name__ == "__main__":
+    candidates = ["Sunday, 7pm until 9pm (In Person)",
+                  "Sunday, 7pm until 9pm (Zoom)",
+                  "Alternating Tuesday/Wednesday 7pm until 9pm (In Person)",
+                  "Alternating Tuesday/Wednesday 7pm until 9pm (Zoom)",
+                  "Friday 6pm until 8pm (In Person)",
+                  "Friday 6pm until 8pm (Zoom)",
+                  "Saturday 11am until 1pm (In Person)",
+                  "Saturday 11am until 1pm (Zoom)"
+                  ]
 
     votes = ["8,7,1,2,6,5,3,4",
              "5,6,1,2,3,4,7,8",
@@ -35,13 +49,4 @@ def run():
              "2,1,4,3,8,7,5,6",
              "1,2,3,4,5,6,7,8",
             ]
-
-    ranked_candidates = [create_ballots(candidates, map(int, ranking_str.split(","))) for ranking_str in votes]
-    ballots = [Ballot(ranked_candidates=ranked_candidate) for ranked_candidate in ranked_candidates]
-
-    election_result = pyrankvote.instant_runoff_voting(candidates, ballots)
-    winners = election_result.get_winners()
-    print(election_result)
-
-if __name__ == "__main__":
-    run()
+    print(get_result(candidates, votes))
